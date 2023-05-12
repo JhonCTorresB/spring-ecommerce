647c6a0af11fe7a1a49cbf1068c215d10bdb1c60
JhonCTorresB <jhontorresbuitron@gmail.com>
2023-05-12 17:01:14 -0500

    crear las clases mmodel, mapeo clases a tablas con JPA, integrando el diseño y vistas

--- src/main/java/com/curso/ecommerce/model/Producto.java
+++ src/main/java/com/curso/ecommerce/model/Producto.java
@@ -1,24 +1,41 @@
 package com.curso.ecommerce.model;
 
-public class Producto {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "productos")
+public class Producto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String 	nombre;
 	private String 	descripcion;
 	private String 	imagen;
 	private double 	precio;
 	private int cantidad;
 	
+	@ManyToOne
+	private Usuario usuario;
+	
 	public Producto() {
 	}
 
-	public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad) {
+	public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad,
+			Usuario usuario) {
 		this.id = id;
 		this.nombre = nombre;
 		this.descripcion = descripcion;
 		this.imagen = imagen;
 		this.precio = precio;
 		this.cantidad = cantidad;
+		this.usuario = usuario;
 	}
 
 	public Integer getId() {
@@ -67,6 +84,14 @@ public class Producto {
 
 	public void setCantidad(int cantidad) {
 		this.cantidad = cantidad;
+	}
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 
 	@Override
--- src/main/java/com/curso/ecommerce/model/Usuario.java
+++ src/main/java/com/curso/ecommerce/model/Usuario.java
@@ -1,8 +1,22 @@
 package com.curso.ecommerce.model;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//<-- hace que sea autoincrementable
 	private Integer id;
+	
 	private String 	nombre;
 	private String 	username;
 	private String 	email;
@@ -10,6 +24,12 @@ public class Usuario {
 	private String 	telefono;
 	private String 	tipo;
 	private String 	password;
+	
+	@OneToMany(mappedBy = "usuario")//<-- lo relaciona con Producto(uno a muchos), mapeado con el campo usuario
+	private List<Producto> productos;
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<Orden> ordenes;
 	
 	public Usuario() {
 	}
@@ -73,6 +93,22 @@ public class Usuario {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
+	
+	public List<Orden> getOrdenes() {
+		return ordenes;
+	}
+
+	public void setOrdenes(List<Orden> ordenes) {
+		this.ordenes = ordenes;
 	}
 
 	@Override
